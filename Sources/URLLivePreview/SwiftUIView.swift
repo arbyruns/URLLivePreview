35345b6ca157711d0df458f6092a49170359e695
Rob Evans <robevans@microsoft.com>
2022-01-06 08:10:14 -0500

    Initial commit

---
+++ Sources/URLLivePreview/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by robevans on 1/6/22.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 13.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
---
+++ Sources/URLLivePreview/URLLivePreview.swift
@@ -0,0 +1,6 @@
+public struct URLLivePreview {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
